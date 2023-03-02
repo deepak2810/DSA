d955e5587f4bffd74efd0276a5bd334cf65a7c7b
deepak2810 <deepakkhanka.97@gmail.com>
2023-03-02 15:07:35 +0530

    typecasting from float to int

---
+++ Tc.java
@@ -0,0 +1,9 @@
+public class Tc {
+    public static void main(String[] args) {
+        //casting
+        //you need to f to tell java its float!
+        float rate = 19.98f;
+        int intRate = (int) rate;
+        System.out.println("Value " + rate + " casted to int = " + intRate);
+    }
+}
