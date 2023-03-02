8391c47f8a2263e5322ce4d5b75ad1095b375516
deepak2810 <deepakkhanka.97@gmail.com>
2023-03-02 14:57:53 +0530

    ...

---
+++ Variab.java
@@ -0,0 +1,13 @@
+public class Variab {
+    public static void main(String[] args) {
+        int my_age = 25;
+        double g = 1.2223445;
+        char c = 'A';
+        String s = "Hello world";
+
+        System.out.println("Deepak this side, I'm"+ my_age + " years of age");
+        System.out.println(g);
+        System.out.println(s);
+
+    }
+}
