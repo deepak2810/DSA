6ffc9ab77b631d0e33a8cad5154991c35ff66032
deepak2810 <deepakkhanka.97@gmail.com>
2023-03-06 14:29:24 +0530

    ..

---
+++ StringsInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class StringsInput {
+    // program illustrates about taking string input in java.
+
+    // 1. Input class.
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //1. Declare variable.
+        String name= input.nextLine(); // nextline() will input entire line. //
+        System.out.println(name);
+
+    }
+
+ }
