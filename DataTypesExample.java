c5e1e3afa1e1613bc7cbe56c70374cbd50e03159
deepak2810 <deepakkhanka.97@gmail.com>
2023-03-06 13:24:57 +0530

    DataTypes in java

---
+++ DataTypesExample.java
@@ -0,0 +1,18 @@
+public class DataTypesExample {
+    public static void main(String[] args) {
+        // declare variables
+        int age =25;
+        double weight = 59.67;
+        boolean isMale = true;
+        char grade = 'A';
+        String name = " Deepak Khanka";
+
+        // print out the variables
+
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+ age);
+        System.out.println("Weight: "+weight);
+        System.out.println("Male: "+ isMale);
+        System.out.println("Grade: "+grade);
+    }
+}
