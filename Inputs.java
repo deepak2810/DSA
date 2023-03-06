fb98b04cd8de18fed7a243a343382d367b3f3c23
deepak2810 <deepakkhanka.97@gmail.com>
2023-03-06 14:06:05 +0530

    ....

---
+++ Inputs.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int employeeCode =  input.nextInt();
+        System.out.println("your employee code is :  "+ employeeCode);
+
+    }
+}
